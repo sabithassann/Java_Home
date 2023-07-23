2ecde5025bec5f862c9fbf077b001a8cadc753a0
sabithassann <sabithassan006@gmail.com>
2023-07-24 00:23:46 +0600

    Homework
    ADD

---
+++ JavaHome22_07_2023/src/javahome22_07_2023/JavaHome22_07_2023.java
@@ -0,0 +1,56 @@
+
+package javahome22_07_2023;
+
+
+public class JavaHome22_07_2023 {
+
+   
+    public static void main(String[] args) {
+        
+        
+//        double formula=(9.5*4.5-2.5*3)/(45.5-3.5);
+//        System.out.println(formula);
+
+//          double n=4 * (1.0 - (1 / 3) + (1 / 5) -
+//						  (1 / 7) + (1 / 9) - (1 / 11));
+//          System.out.println(n);
+
+//         double pi=Math.PI;
+//         double perimeter=2*5.5*pi;
+//         double area=5.5*5.5*pi;
+//         
+//         System.out.println("perimeter "+ perimeter);
+//         System.out.println("area "+ area);
+
+
+           
+//          double miles=14/1.6;
+//          System.out.println(miles);
+//          
+//          double perhour=miles/60;
+//          System.out.println(perhour);
+          
+//          System.out.println((14/45.30)/1.6);
+
+
+            int birth=31536000/7;
+            int death=31536000/13;
+            int immigrant=31536000/45;
+            
+            System.out.println("birth "+birth+" death "+death+" immigrant "+immigrant);
+          
+            
+          
+          int result=(birth+immigrant)-death;
+          System.out.println(result);
+          
+          int inFiveYears=312032486+(result*5);
+          System.out.println("After five years the population will be "+inFiveYears);
+         
+         
+         
+           
+
+    }
+    
+}
